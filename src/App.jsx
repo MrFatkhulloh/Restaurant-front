a6d6efc87b06a2e548f8497cddbc6ea96065783d
Fatkhulloh <eternallfame@gmail.com>
2022-08-10 02:41:10 +0500

    adding home path to make netlify work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Admin from "./Components/Admin/Admin";
 import Home from "./Components/Home/Home";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
 
 	return <div className="App">
     <Routes>
+      <Route path={"/*"} element={<Navigate to={"/Home"}/>}/>
       <Route path="/Home/*" element={<Home/>} />
       <Route path="/Admin/*" element={<Admin/>} />
     </Routes>
